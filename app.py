812990d1572f9e69503a79fc33e09f64fa8a029b
Pragati Tiwari <pragati015tiwari@gmail.com>
2025-01-17 13:48:58 +0000

    added dynamic and API route

--- app.py
+++ app.py
@@ -1,8 +1,31 @@
-from flask import Flask
-app =Flask(__name__)
-@app.route("/")
-def hellow_world():
-  return"<p>Hello,pragati!</p>"
+from flask import Flask, render_template, jsonify 
 
-if __name__=="__main__":
-  app.run(host='0.0.0.0',debug=True)
+app = Flask(__name__)
+
+plants=[
+
+  {
+    'id': 1,
+    'title': 'plant 1',
+    'breed':'breed 1',
+    'disease': 'disease 1',
+  },
+  {
+    'id': 2,
+    'title': 'plant 2',
+    'breed':'breed 2',
+    'disease': 'disease 2',
+  },
+]
+
+@app.route("/")
+def hello_plants():
+  return render_template('home.html', plants=plants, plant_name='plant')
+
+@app.route("/api/plants")
+def list_plants():
+  return jsonify(plants)
+
+
+if __name__ == "__main__":
+  app.run(host='0.0.0.0', debug=True)
