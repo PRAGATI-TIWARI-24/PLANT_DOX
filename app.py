cf595008f1500e3adaaa6308e1009d91bc309edb
Pragati Tiwari <pragati015tiwari@gmail.com>
2025-01-23 05:58:33 +0000

    added css style

--- app.py
+++ app.py
@@ -2,25 +2,38 @@ from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
 
-plants=[
+PLANTS=[
 
   {
     'id': 1,
-    'title': 'plant 1',
-    'breed':'breed 1',
-    'disease': 'disease 1',
+    'title': 'lotus',
+    'breed': 'Nelumbo lutea',
+    'disease': 'Leaf Spot Disease',
   },
+  
   {
     'id': 2,
-    'title': 'plant 2',
-    'breed':'breed 2',
-    'disease': 'disease 2',
+    'title': 'rose',
+    'breed': 'Mr. Lincoln',
+    'disease': 'Black Spot',
+  },
+  {
+    'id': 3,
+    'title': 'sunflower',
+    'breed': 'Mammoth',
+    'disease': 'Downy Mildew',
+  },
+  {
+    'id': 4,
+    'title': 'daisy',
+    'breed': 'Shasta Daisy',
+    'disease': 'Powdery Mildew',
   },
 ]
 
 @app.route("/")
 def hello_plants():
-  return render_template('home.html', plants=plants, plant_name='plant')
+  return render_template('home.html', plants=PLANTS, plant_name='PLANT')
 
 @app.route("/api/plants")
 def list_plants():
